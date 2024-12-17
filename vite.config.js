ffe841a5f5045a45a70d319807d721b99dbf24af
theiereman <thomas.eyermann@protonmail.com>
2024-12-17 16:02:26 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
+import ViteRestart from "vite-plugin-restart";
+
 export default {
+  root: "src/",
+  publicDir: "../static/",
   server: {
     open: true,
   },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [
+    ViteRestart({
+      restart: ["../static/**"],
+    }),
+  ],
 };
